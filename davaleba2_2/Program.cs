65a72f71a354d10a6395fe6acd28c7cf0bb7dc76
NikolozK34 <kekelidze97@gmail.com>
2023-12-01 17:02:23 +0400

    Add project files.

---
+++ davaleba2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sheyvane asaki; ");
+
+            int asaki = int.Parse(Console.ReadLine());
+
+            if(asaki >=18) 
+            {
+                Console.WriteLine("gilocav! xmis micemis ufleba gaqvs");
+            }else if(asaki <18) 
+            {
+                Console.WriteLine("samwuxarod xmis micemis ufleba jer ar gaqvs");
+            }
+            else 
+            {
+                Console.WriteLine("wrong input");
+            }
+        }
+    }
+}
